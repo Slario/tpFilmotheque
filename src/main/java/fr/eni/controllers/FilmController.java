cb0699cbfaf95cde5eb472c54f9bbd32a2ff3b6d
Slario <nicolas.legendre.ter@gmail.com>
2021-11-19 11:27:06 +0100

    indexfilms
    detail film

--- src/main/java/fr/eni/controllers/FilmController.java
+++ src/main/java/fr/eni/controllers/FilmController.java
@@ -1,18 +1,41 @@
 package fr.eni.controllers;
 
+import java.util.ArrayList;
+import java.util.Date;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.SessionAttributes;
+
+import fr.eni.bo.Film;
 
 @Controller
 @RequestMapping("/film")
+@SessionAttributes("films")
 public class FilmController {
 	
 	
+	@ModelAttribute("films")
+	public ArrayList<Film> getFilms(){
+		ArrayList<Film> retour = new ArrayList<Film>();
+		Film terminator = new Film("Terminator", new Date(), 120);
+		Film matrix = new Film("Matrix", new Date(), 180);
+		Film tarzan = new Film("Tarzan", new Date(), 150);
+
+		retour.add(terminator);
+		retour.add(matrix);
+		retour.add(tarzan);
+		
+		return retour;
+	}
+	
+	@GetMapping({"/", "/index", ""})
 	public String index(Model model) {
 		
-		
-		return 
+		return "Film/index";
 	}
 
 }
