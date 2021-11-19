ef836caa27d799b15662deb23bbd5ea877491fc5
Slario <nicolas.legendre.ter@gmail.com>
2021-11-19 09:49:25 +0100

    indexFilm
    deplaçage de la vue dans les templates (oopsy)

--- src/main/java/fr/eni/controllers/FilmController.java
+++ src/main/java/fr/eni/controllers/FilmController.java
@@ -1,10 +1,18 @@
 package fr.eni.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @RequestMapping("/film")
 public class FilmController {
+	
+	
+	public String index(Model model) {
+		
+		
+		return 
+	}
 
 }
